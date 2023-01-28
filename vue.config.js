9616959e5ef522db1669e615fdd845272e22629e
andrew <ancam1987@gmail.com>
2023-01-28 13:08:41 +0200

    Added PWA ability

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/petanque-swiss-vue/dist/'
-        : '/'
+        : '/',
+    pwa: {
+        name: 'Petanque Draw',
+        themeColor: '#471aa0',
+        msTileColor: '#471aa0'
+    }
 }

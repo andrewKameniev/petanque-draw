be00a68a9eff837e67a555f25cdba14dae097783
andrew <ancam1987@gmail.com>
2023-01-28 13:11:56 +0200

    Added PWA ability

---
+++ dist/precache-manifest.4c61531066a5a8126821d468d981412c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec5bcde0da91ec18bb4",
+    "url": "/petanque-swiss-vue/dist/css/app.2c2413e3.css"
+  },
+  {
+    "revision": "811ee66ee938eb02f91d7984efcc06fc",
+    "url": "/petanque-swiss-vue/dist/img/bg.811ee66e.jpg"
+  },
+  {
+    "revision": "70b28108b4212197a91e174bfba8a843",
+    "url": "/petanque-swiss-vue/dist/img/logo.70b28108.webp"
+  },
+  {
+    "revision": "ec2cab1b9b53c7b057cc374ebcf1a7ec",
+    "url": "/petanque-swiss-vue/dist/img/logo.ec2cab1b.png"
+  },
+  {
+    "revision": "a6db09294f7ed1807c2a23b86f5f3ea0",
+    "url": "/petanque-swiss-vue/dist/index.html"
+  },
+  {
+    "revision": "cec5bcde0da91ec18bb4",
+    "url": "/petanque-swiss-vue/dist/js/app.435b4fea.js"
+  },
+  {
+    "revision": "bc132b67a7697d38d7fe",
+    "url": "/petanque-swiss-vue/dist/js/chunk-vendors.6797b2a8.js"
+  },
+  {
+    "revision": "aa7682708a7f641ef8a7eb9eb0aed300",
+    "url": "/petanque-swiss-vue/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/petanque-swiss-vue/dist/robots.txt"
+  }
+]);

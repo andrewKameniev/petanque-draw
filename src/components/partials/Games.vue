andrewKameniev/petanque-draw

commit 17e19679fff67e5fa0980b48c64735f1af9b5fe5

Resolved issue with technical lanes

src/components/partials/Games.vue
@@ -28,7 +28,7 @@
                     <Game v-for="(game, index) in tournament.games[activeRound - 1]" :key="index"
                           :game="game" :activeRound="activeRound - 1" :compactView="compactView" :game-index="index"
                           :team1-lanes="tournament.teams.find(team => team.title === game.team_1).lanes"
-                          :team2-lanes="tournament.teams.find(team => team.title === game.team_2).lanes"
+                          :team2-lanes="tournament.teams.find(team => team.title === game.team_2)?.lanes || null"
                           @save="saveResults"/>
                     <div v-if="scoreError" class="has-text-centered has-text-danger mb-5">{{ $t('games.resultsError') }}
                     </div>
@@ -386,12 +386,14 @@ export default {
             this.tournament.teams.forEach(team => {
                 teamsMatrix[team.title] = {};
                 for (let i = firstlane; i < Math.floor(firstlane + this.tournament.teams.length / 2); i++) {
-                    teamsMatrix[team.title][i] = 0;
-                    team.lanes.forEach(lane => {
-                        if (i === lane) {
-                            teamsMatrix[team.title][i]++;
-                        }
-                    })
+                    if (team.lanes && team.lanes.length) {
+                        teamsMatrix[team.title][i] = 0;
+                        team.lanes.forEach(lane => {
+                            if (i === lane) {
+                                teamsMatrix[team.title][i]++;
+                            }
+                        })
+                    }
                 }
             })
 

dist/index.html
@@ -1,1 +1,1 @@
-<!doctype html><html lang="en" prefix="og: https://ogp.me/ns#"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="title" content="Petanque Draw"/><meta name="description" content="Program for drawing petanque tournaments: swiss & round & knockout systems"/><meta property="og:site_name" content="Petanque Draw"/><meta property="og:title" content="Program for drawing petanque tournaments: swiss & round & knockout systems"/><meta property="og:type" content="website"/><meta property="og:image" content="/petanque-draw/dist//img/logo.png"/><meta name="twitter:card" content="summary_large_image"/><meta name="viewport" content="width=device-width,initial-scale=1"><!--[if IE]><link rel="icon" href="/petanque-draw/dist/favicon.ico"><![endif]--><title>Petanque Draw</title><script defer="defer" src="/petanque-draw/dist/js/chunk-vendors.172067e5.js"></script><script defer="defer" src="/petanque-draw/dist/js/app.cc8e2093.js"></script><link href="/petanque-draw/dist/css/chunk-vendors.210a7e65.css" rel="stylesheet"><link href="/petanque-draw/dist/css/app.6e923d03.css" rel="stylesheet"><link rel="icon" type="image/svg+xml" href="/petanque-draw/dist/img/icons/favicon.svg"><link rel="icon" type="image/png" sizes="32x32" href="/petanque-draw/dist/img/icons/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/petanque-draw/dist/img/icons/favicon-16x16.png"><link rel="manifest" href="/petanque-draw/dist/manifest.json"><meta name="theme-color" content="#471aa0"><meta name="apple-mobile-web-app-capable" content="no"><meta name="apple-mobile-web-app-status-bar-style" content="default"><meta name="apple-mobile-web-app-title" content="Petanque Draw"><link rel="apple-touch-icon" href="/petanque-draw/dist/img/icons/apple-touch-icon-152x152.png"><link rel="mask-icon" href="/petanque-draw/dist/img/icons/safari-pinned-tab.svg" color="#471aa0"><meta name="msapplication-TileImage" content="/petanque-draw/dist/img/icons/msapplication-icon-144x144.png"><meta name="msapplication-TileColor" content="#471aa0"></head><body><noscript><strong>We're sorry but petanque-draw doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>
+<!doctype html><html lang="en" prefix="og: https://ogp.me/ns#"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="title" content="Petanque Draw"/><meta name="description" content="Program for drawing petanque tournaments: swiss & round & knockout systems"/><meta property="og:site_name" content="Petanque Draw"/><meta property="og:title" content="Program for drawing petanque tournaments: swiss & round & knockout systems"/><meta property="og:type" content="website"/><meta property="og:image" content="/petanque-draw/dist//img/logo.png"/><meta name="twitter:card" content="summary_large_image"/><meta name="viewport" content="width=device-width,initial-scale=1"><!--[if IE]><link rel="icon" href="/petanque-draw/dist/favicon.ico"><![endif]--><title>Petanque Draw</title><script defer="defer" src="/petanque-draw/dist/js/chunk-vendors.172067e5.js"></script><script defer="defer" src="/petanque-draw/dist/js/app.6b356992.js"></script><link href="/petanque-draw/dist/css/chunk-vendors.210a7e65.css" rel="stylesheet"><link href="/petanque-draw/dist/css/app.6e923d03.css" rel="stylesheet"><link rel="icon" type="image/svg+xml" href="/petanque-draw/dist/img/icons/favicon.svg"><link rel="icon" type="image/png" sizes="32x32" href="/petanque-draw/dist/img/icons/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="/petanque-draw/dist/img/icons/favicon-16x16.png"><link rel="manifest" href="/petanque-draw/dist/manifest.json"><meta name="theme-color" content="#471aa0"><meta name="apple-mobile-web-app-capable" content="no"><meta name="apple-mobile-web-app-status-bar-style" content="default"><meta name="apple-mobile-web-app-title" content="Petanque Draw"><link rel="apple-touch-icon" href="/petanque-draw/dist/img/icons/apple-touch-icon-152x152.png"><link rel="mask-icon" href="/petanque-draw/dist/img/icons/safari-pinned-tab.svg" color="#471aa0"><meta name="msapplication-TileImage" content="/petanque-draw/dist/img/icons/msapplication-icon-144x144.png"><meta name="msapplication-TileColor" content="#471aa0"></head><body><noscript><strong>We're sorry but petanque-draw doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript><div id="app"></div></body></html>

dist/service-worker.js
@@ -1,2 +1,2 @@
-if(!self.define){let e,i={};const n=(n,r)=>(n=new URL(n+".js",r).href,i[n]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=i,document.head.appendChild(e)}else e=n,importScripts(n),i()})).then((()=>{let e=i[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(r,a)=>{const s=e||("document"in self?document.currentScript.src:"")||location.href;if(i[s])return;let u={};const l=e=>n(e,s),t={module:{uri:s},exports:u,require:l};i[s]=Promise.all(r.map((e=>t[e]||l(e)))).then((e=>(a(...e),u)))}}define(["./workbox-6567b62a"],(function(e){"use strict";e.setCacheNameDetails({prefix:"petanque-draw"}),self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.precacheAndRoute([{url:"/petanque-draw/dist/css/app.6e923d03.css",revision:null},{url:"/petanque-draw/dist/css/chunk-vendors.210a7e65.css",revision:null},{url:"/petanque-draw/dist/firebase-messaging-sw.js",revision:"397ec80a8e18f2221b659e49a4ca40f7"},{url:"/petanque-draw/dist/img/Doc1.fe712fee.png",revision:null},{url:"/petanque-draw/dist/img/Doc10.eaa1f308.png",revision:null},{url:"/petanque-draw/dist/img/Doc11.a8f37edd.png",revision:null},{url:"/petanque-draw/dist/img/Doc12.8108252b.png",revision:null},{url:"/petanque-draw/dist/img/Doc13.deda737b.png",revision:null},{url:"/petanque-draw/dist/img/Doc2.ddef2f60.png",revision:null},{url:"/petanque-draw/dist/img/Doc3.26b463aa.png",revision:null},{url:"/petanque-draw/dist/img/Doc4.5ff56e55.png",revision:null},{url:"/petanque-draw/dist/img/Doc5.a4d0a45d.png",revision:null},{url:"/petanque-draw/dist/img/Doc7.94b39e80.png",revision:null},{url:"/petanque-draw/dist/img/Doc8.84244763.png",revision:null},{url:"/petanque-draw/dist/img/Doc9.5f95affc.png",revision:null},{url:"/petanque-draw/dist/img/Train1.c2331816.png",revision:null},{url:"/petanque-draw/dist/img/Train2.615c0f91.png",revision:null},{url:"/petanque-draw/dist/img/Train3.c12cc4dd.png",revision:null},{url:"/petanque-draw/dist/img/Train4.99f1840e.png",revision:null},{url:"/petanque-draw/dist/img/Train5.d6f449dc.png",revision:null},{url:"/petanque-draw/dist/img/bg-mountains.28a9e65d.avif",revision:null},{url:"/petanque-draw/dist/img/bg.1eca2192.avif",revision:null},{url:"/petanque-draw/dist/img/bg.e7592ead.jpg",revision:null},{url:"/petanque-draw/dist/img/girl.5c1e06cd.jpg",revision:null},{url:"/petanque-draw/dist/img/login-desktop.70f80e89.jpg",revision:null},{url:"/petanque-draw/dist/img/login-mobile.03a9c97e.jpg",revision:null},{url:"/petanque-draw/dist/img/logo.6a11ce3e.webp",revision:null},{url:"/petanque-draw/dist/img/logo.74972a48.png",revision:null},{url:"/petanque-draw/dist/img/stat1.46c72e52.png",revision:null},{url:"/petanque-draw/dist/img/stat10.478a1fca.png",revision:null},{url:"/petanque-draw/dist/img/stat2.e4ac8782.png",revision:null},{url:"/petanque-draw/dist/img/stat3.79037012.png",revision:null},{url:"/petanque-draw/dist/img/stat4.5abbaa2f.png",revision:null},{url:"/petanque-draw/dist/img/stat5.ef5655a7.png",revision:null},{url:"/petanque-draw/dist/img/stat6.daa8f0f1.png",revision:null},{url:"/petanque-draw/dist/img/stat7.63497109.png",revision:null},{url:"/petanque-draw/dist/img/stat8.cf3c6537.png",revision:null},{url:"/petanque-draw/dist/img/stat9.52da0ff2.png",revision:null},{url:"/petanque-draw/dist/index.html",revision:"10819d9eddf55e7e853dd8f01a7e51ee"},{url:"/petanque-draw/dist/js/668.26668aff.js",revision:null},{url:"/petanque-draw/dist/js/838.f5fbcf69.js",revision:null},{url:"/petanque-draw/dist/js/app.cc8e2093.js",revision:null},{url:"/petanque-draw/dist/js/chunk-vendors.172067e5.js",revision:null},{url:"/petanque-draw/dist/manifest.json",revision:"aa7682708a7f641ef8a7eb9eb0aed300"},{url:"/petanque-draw/dist/robots.txt",revision:"b6216d61c03e6ce0c9aea6ca7808f7ca"}],{})}));
+if(!self.define){let e,i={};const n=(n,r)=>(n=new URL(n+".js",r).href,i[n]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=n,e.onload=i,document.head.appendChild(e)}else e=n,importScripts(n),i()})).then((()=>{let e=i[n];if(!e)throw new Error(`Module ${n} didn’t register its module`);return e})));self.define=(r,a)=>{const s=e||("document"in self?document.currentScript.src:"")||location.href;if(i[s])return;let u={};const l=e=>n(e,s),t={module:{uri:s},exports:u,require:l};i[s]=Promise.all(r.map((e=>t[e]||l(e)))).then((e=>(a(...e),u)))}}define(["./workbox-6567b62a"],(function(e){"use strict";e.setCacheNameDetails({prefix:"petanque-draw"}),self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.precacheAndRoute([{url:"/petanque-draw/dist/css/app.6e923d03.css",revision:null},{url:"/petanque-draw/dist/css/chunk-vendors.210a7e65.css",revision:null},{url:"/petanque-draw/dist/firebase-messaging-sw.js",revision:"397ec80a8e18f2221b659e49a4ca40f7"},{url:"/petanque-draw/dist/img/Doc1.fe712fee.png",revision:null},{url:"/petanque-draw/dist/img/Doc10.eaa1f308.png",revision:null},{url:"/petanque-draw/dist/img/Doc11.a8f37edd.png",revision:null},{url:"/petanque-draw/dist/img/Doc12.8108252b.png",revision:null},{url:"/petanque-draw/dist/img/Doc13.deda737b.png",revision:null},{url:"/petanque-draw/dist/img/Doc2.ddef2f60.png",revision:null},{url:"/petanque-draw/dist/img/Doc3.26b463aa.png",revision:null},{url:"/petanque-draw/dist/img/Doc4.5ff56e55.png",revision:null},{url:"/petanque-draw/dist/img/Doc5.a4d0a45d.png",revision:null},{url:"/petanque-draw/dist/img/Doc7.94b39e80.png",revision:null},{url:"/petanque-draw/dist/img/Doc8.84244763.png",revision:null},{url:"/petanque-draw/dist/img/Doc9.5f95affc.png",revision:null},{url:"/petanque-draw/dist/img/Train1.c2331816.png",revision:null},{url:"/petanque-draw/dist/img/Train2.615c0f91.png",revision:null},{url:"/petanque-draw/dist/img/Train3.c12cc4dd.png",revision:null},{url:"/petanque-draw/dist/img/Train4.99f1840e.png",revision:null},{url:"/petanque-draw/dist/img/Train5.d6f449dc.png",revision:null},{url:"/petanque-draw/dist/img/bg-mountains.28a9e65d.avif",revision:null},{url:"/petanque-draw/dist/img/bg.1eca2192.avif",revision:null},{url:"/petanque-draw/dist/img/bg.e7592ead.jpg",revision:null},{url:"/petanque-draw/dist/img/girl.5c1e06cd.jpg",revision:null},{url:"/petanque-draw/dist/img/login-desktop.70f80e89.jpg",revision:null},{url:"/petanque-draw/dist/img/login-mobile.03a9c97e.jpg",revision:null},{url:"/petanque-draw/dist/img/logo.6a11ce3e.webp",revision:null},{url:"/petanque-draw/dist/img/logo.74972a48.png",revision:null},{url:"/petanque-draw/dist/img/stat1.46c72e52.png",revision:null},{url:"/petanque-draw/dist/img/stat10.478a1fca.png",revision:null},{url:"/petanque-draw/dist/img/stat2.e4ac8782.png",revision:null},{url:"/petanque-draw/dist/img/stat3.79037012.png",revision:null},{url:"/petanque-draw/dist/img/stat4.5abbaa2f.png",revision:null},{url:"/petanque-draw/dist/img/stat5.ef5655a7.png",revision:null},{url:"/petanque-draw/dist/img/stat6.daa8f0f1.png",revision:null},{url:"/petanque-draw/dist/img/stat7.63497109.png",revision:null},{url:"/petanque-draw/dist/img/stat8.cf3c6537.png",revision:null},{url:"/petanque-draw/dist/img/stat9.52da0ff2.png",revision:null},{url:"/petanque-draw/dist/index.html",revision:"a0ed4a9baa629fe01bc0849241f6ad2f"},{url:"/petanque-draw/dist/js/668.26668aff.js",revision:null},{url:"/petanque-draw/dist/js/838.f5fbcf69.js",revision:null},{url:"/petanque-draw/dist/js/app.6b356992.js",revision:null},{url:"/petanque-draw/dist/js/chunk-vendors.172067e5.js",revision:null},{url:"/petanque-draw/dist/manifest.json",revision:"aa7682708a7f641ef8a7eb9eb0aed300"},{url:"/petanque-draw/dist/robots.txt",revision:"b6216d61c03e6ce0c9aea6ca7808f7ca"}],{})}));
 //# sourceMappingURL=service-worker.js.map

commit 27f72572ad4c2f0beaebb222624411c2078d08dc

Fix lane assignment regression from 17e1967
Commit 17e1967 (technical lanes fix) accidentally moved the matrix
initialization `teamsMatrix[team.title][i] = 0` inside the
`if (team.lanes.length)` guard. This meant on round 1 (empty lanes)
the matrix stayed empty, weights computed as NaN, and no lanes were
ever assigned — breaking the no-repeat algorithm for all rounds.

Fix: initialize matrix to 0 unconditionally, only count history
inside the guard.

Also fixed shuffleLanes to use assignLanes instead of pure random
shuffle, so the no-repeat constraint is respected when reshuffling.

Co-Authored-By: Claude Opus 4.6 <noreply@anthropic.com>

src/components/partials/Games.vue
@@ -85,8 +85,9 @@ export default {
         ...mapMutations(['startRound', 'endRound', 'addRoundToGames', 'restoreRound', 'showMessage', 'shuffleLanesStore']),
         gameHasError,
         shuffleLanes() {
-            this.tournament.games[this.tournament.games.length - 1] = shuffleArray(this.tournament.games[this.tournament.games.length - 1]);
-            this.shuffleLanesStore(this.tournament.games[this.tournament.games.length - 1]);
+            const currentRound = this.tournament.games[this.tournament.games.length - 1];
+            const reshuffled = this.assignLanes(shuffleArray([...currentRound]));
+            this.shuffleLanesStore(reshuffled);
         },
         getRandomWithOneExclusion(lengthOfArray, indexToExclude1 = null, indexToExclude2 = null) { // для определения рандомного соперника, если жеребим не по рейтингу
             let rand = null;
@@ -386,8 +387,8 @@ export default {
             this.tournament.teams.forEach(team => {
                 teamsMatrix[team.title] = {};
                 for (let i = firstlane; i < Math.floor(firstlane + this.tournament.teams.length / 2); i++) {
+                    teamsMatrix[team.title][i] = 0;
                     if (team.lanes && team.lanes.length) {
-                        teamsMatrix[team.title][i] = 0;
                         team.lanes.forEach(lane => {
                             if (i === lane) {
                                 teamsMatrix[team.title][i]++;
cae2b6996fd125f67ed318336523f6faf37baa8b
nemo15 <nemo15.alex@gmail.com>
2026-05-15 00:30:02 +0300

    Fix bracket button: ensure icon renders visibly

--- src/components/partials/Results.vue
+++ src/components/partials/Results.vue
@@ -26,8 +26,8 @@
                             {{ $t('results.all') }}
                         </button>
                     </div>
-                    <button v-if="hasPlayOffResults && !isForProtocol" class="button is-small btn-purple-outline mb-1 btn-bracket" @click="showBracket = true">
-                        <GitFork :size="14" style="transform: rotate(90deg); margin-right: 0.3rem;"/>
+                    <button v-if="hasPlayOffResults && !isForProtocol" class="button is-small btn-purple-outline mb-1" @click="showBracket = true">
+                        <GitFork :size="16" :stroke-width="2" style="transform: rotate(90deg); margin-right: 0.3rem; min-width: 16px;"/>
                         <span class="is-hidden-mobile">{{ $t('games.showBracket') }}</span>
                     </button>
                 </div>
